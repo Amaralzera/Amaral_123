28a180eed0f45cc6f8ee2c2d2c5ff143f029f7d4
Amaralzera <100435948+Amaralzera@users.noreply.github.com>
2023-08-17 15:08:30 -0300

    a
    a

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('register') }}">
+        <form method="POST" action="{{ route('register-movie') }}">
             @csrf
 
             <!-- Name -->
@@ -35,16 +35,6 @@
                                 name="password"
                                 required autocomplete="new-password" />
             </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
